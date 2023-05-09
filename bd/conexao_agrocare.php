2c5f3730e2d94db9589bb64c6f62e4dc37094838
Bia <beatriz.petry@outlook.com>
2023-05-09 17:20:40 -0300

    ajustes php

--- bd/conexao_agrocare.php
+++ bd/conexao_agrocare.php
@@ -1,14 +1,20 @@
 
 <?php
-global $servername ;
-global $username;
-global $password;
-global $database;
+    global $servername ;
+    global $username;
+    global $password;
+    global $database;
 
-$servername = "localhost:3306";
-$username = "agrocare@TI";
-$password = "projeto@agrocare";
-$database = "agrocarefinal";
+    $servername = "localhost:3306";
+    $username = "agrocare@TI";
+    $password = "projeto@agrocare";
+    $database = "agrocarefinal";
+
+
+    $connectbd=mysqli_connect($servername, $username, $password, $database);
+    if(!$connectbd) {
+        die('Erro de conexão: '.mysqli_connect_error());
+    } 
 ?>
 
 
