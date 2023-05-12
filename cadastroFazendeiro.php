c658de13c5ee1854a4919f58f935bfacb4297162
GUILHERME VILELA <107810558+guizaolopes@users.noreply.github.com>
2023-05-12 14:43:56 -0300

    Update cadastroFazendeiro.php

--- cadastroFazendeiro.php
+++ cadastroFazendeiro.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Fazendeiro</title>
-    <link rel="stylesheet"  type="text/css" href="style.css">
+    <link rel="stylesheet"  type="text/css" href="style/cadastroFazenda.css">
 </head>
 <header>
    <img src="logo.png" width="180px">
