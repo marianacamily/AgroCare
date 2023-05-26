c201446512ed269dbad610bd163ec1e632a8ad43
marianacamily <mariana.camily@outlook.com>
2023-05-26 10:19:23 -0300

    Alteração telas de cadastro de Vacas

---
+++ scripts/scriptCadVacas.js
@@ -0,0 +1,6 @@
+let menuBox = document.getElementById("menuBox");
+let menuButton = document.getElementById("menuButton");
+
+menuButton.onclick = function () {
+    menuBox.classList.toggle("open-menu");
+}
