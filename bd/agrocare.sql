d72b30fe0d38e9326d79eaa6105a380da3758c7b
Bia <beatriz.petry@outlook.com>
2023-05-27 14:22:55 -0300

    ajustes no SQL

--- bd/agrocare.sql
+++ bd/agrocare.sql
@@ -2,52 +2,74 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-/* para colocar o código no phpMyAdmin é necessário tirar todas as aspas ~ Bia */
-
-CREATE TABLE 'Fazendeiro' (
-	'cpf_Fazendeiro' char(14) PRIMARY KEY NOT NULL, 
-	'nome_Fazendeiro' varchar(50) NOT NULL,
-	'dt_nascFazendeiro' Date NOT NULL,
-	'telefone_Fazendeiro' char(14) NOT NULL,
-	'email_Fazendeiro' varchar(50) NOT NULL,
-	'senha_Fazendeiro' varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/* criação da tabela fazendeiro */
-
-CREATE TABLE 'Veterinário' (
-	'cpf_Vet' char(14) PRIMARY KEY NOT NULL, 
-	'nome_Vet' varchar(50) NOT NULL,
-	'dt_nascVet' Date NOT NULL,
-	'telefone_Vet' char(14) NOT NULL,
-	'email_Vet' varchar(50) NOT NULL,
-	'senha_Vet' varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/* criação da tabela veterinário */
-
-CREATE TABLE 'Funcionário' (
-	'cpf_Func' char(14) PRIMARY KEY NOT NULL, 
-	'nome_Func' varchar(50) NOT NULL,
-	'dt_nascFunc' Date NOT NULL,
-	'telefone_Func' char(14) NOT NULL,
-	'email_Func' varchar(50) NOT NULL,
-	'senha_Func' varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE Funcionário (
+	cpf_Func char(14) PRIMARY KEY NOT NULL, 
+	nome_Func varchar(50) NOT NULL,
+	dt_nascFunc Date NOT NULL,
+	telefone_Func char(14) NOT NULL,
+	email_Func varchar(50) NOT NULL,
+	senha_Func varchar(50) NOT NULL
+);
 /* criação da tabela funcionário */
 
-CREATE TABLE 'Fazenda' (
-	'id_Fazenda' int PRIMARY KEY NOT NULL,
-	'nome_Fazenda' varchar(50) NOT NULL,
-	'endereço_Fazenda' varchar(50) NOT NULL,
-    'bairro_Fazenda' varchar(30) NOT NULL,
-    'numero_Fazenda' varchar(10) NOT NULL,
-    'cidade_Fazenda' varchar(30) NOT NULL,
-    'estado_Fazenda' varchar(15) NOT NULL,
-    'cep_Fazenda' varchar(9) NOT NULL,
-	'FK_cpf_Fazendeiro' char(14) NOT NULL, FOREIGN KEY ('FK_cpf_Fazendeiro') REFERENCES 'Fazendeiro' ('cpf_Fazendeiro'),
-	'FK_cpf_Vet' char(14) NOT NULL, FOREIGN KEY ('FK_cpf_Vet') REFERENCES Veterinário ('cpf_Vet'),
-	'FK_cpf_Func' char(14) NOT NULL, FOREIGN KEY ('FK_cpf_Func') REFERENCES Funcionário ('cpf_Func')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE Fazenda (
+	id_Fazenda int PRIMARY KEY NOT NULL,
+	nome_Fazenda varchar(50) NOT NULL,
+	endereço_Fazenda varchar(50) NOT NULL,
+    bairro_Fazenda varchar(30) NOT NULL,
+    numero_Fazenda varchar(10) NOT NULL,
+    cidade_Fazenda varchar(30) NOT NULL,
+    estado_Fazenda varchar(15) NOT NULL,
+    cep_Fazenda varchar(9) NOT NULL,
+	FK_cpf_Func char(14), FOREIGN KEY (FK_cpf_Func) REFERENCES Funcionário (cpf_Func)
+);
 /* criação da tabela fazenda com as FKs vindas das outras tabelas (os cpfs)*/
 
-ALTER TABLE 'Fazenda' MODIFY 'id_Fazenda' int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-/* fazendo com que cada id novo de uma fazenda seja incrementado por 15 */
+ALTER TABLE Fazenda MODIFY id_Fazenda int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+CREATE TABLE Fazendeiro (
+	cpf_Fazendeiro char(14) PRIMARY KEY NOT NULL, 
+	nome_Fazendeiro varchar(50) NOT NULL,
+	dt_nascFazendeiro Date NOT NULL,
+	telefone_Fazendeiro char(14) NOT NULL,
+	email_Fazendeiro varchar(50) NOT NULL,
+	senha_Fazendeiro varchar(50) NOT NULL,
+	FK_id_Fazenda int, FOREIGN KEY (FK_id_Fazenda) REFERENCES Fazenda (id_Fazenda)
+); 
+/* criação da tabela fazendeiro */
+
+CREATE TABLE Veterinário (
+	cpf_Vet char(14) PRIMARY KEY NOT NULL, 
+	nome_Vet varchar(50) NOT NULL,
+	dt_nascVet Date NOT NULL,
+	telefone_Vet char(14) NOT NULL,
+	email_Vet varchar(50) NOT NULL,
+	senha_Vet varchar(50) NOT NULL,
+    FK_id_Fazenda int, FOREIGN KEY (FK_id_Fazenda) REFERENCES Fazenda (id_Fazenda)
+);
+/* criação da tabela veterinário */
+
+
+CREATE TABLE Vaca (
+    num_ID_Vaca varchar(10) PRIMARY KEY NOT NULL,
+    data_Nasc_Vaca Date NOT NULL,
+    raça_Vaca varchar(25) NOT NULL,
+    FK_id_Fazenda int, FOREIGN KEY (FK_id_Fazenda) REFERENCES Fazenda (id_Fazenda)
+);
+
+/*Não está Funcionando*/
+CREATE TABLE Login_Fazendeiro (
+    id_Login_Faz int PRIMARY KEY NOT NULL,
+    FK_id_Fazenda int, FOREIGN KEY (FK_id_Fazenda) REFERENCES Fazenda (id_Fazenda),
+    FK_email_Fazendeiro varchar(50), FOREIGN KEY (FK_email_Fazendeiro) REFERENCES Fazendeiro (email_Fazendeiro)
+);
+
+ALTER TABLE Login_Fazendeiro MODIFY id_Login_Faz int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+CREATE TABLE Login_Veterinário (
+    id_Login_Vet int PRIMARY KEY NOT NULL,
+    FK_id_Fazenda int, FOREIGN KEY (FK_id_Fazenda) REFERENCES Fazenda (id_Fazenda),
+    FK_email_Vet varchar(50), FOREIGN KEY (FK_email_Vet) REFERENCES Veterinário (email_Vet)
+);
+
+ALTER TABLE Login_Veterinário MODIFY id_Login_Vet int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
